d95b4407988bc5c98c29dcae1fabc01cd6ea6326
Ajay chouhan <51070371+ajaychouhan123@users.noreply.github.com>
2024-01-17 14:50:06 +0530

    Upload Product.cs

--- Model/Product.cs
+++ Model/Product.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
-namespace ajaychouhan1.Model
+namespace ajay_chouhan.Model
 {
     public class Product
     {
